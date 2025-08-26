6606430d23926482df4dfd3d1721fa918eeaf12e
Bárbara Azevedo <barbara.azevedo22@gmail.com>
2025-08-26 19:17:33 -0300

    desafio 02

--- Program.cs
+++ Program.cs
@@ -4,21 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Pessoa pessoa1 = new Pessoa("Bárbara", 25, "Desenvolvedora");
-            Pessoa pessoa2 = new Pessoa("Karina", 30, "Arquiteta");
+            Retangulo retangulo = new Retangulo(5.5, 10.2);
 
-            Carro carro1 = new Carro("Ford", "Mustang", 1969);
-            Carro carro2 = new Carro("Volkswagen", "Fusca", 1970);
-            Carro carro3 = new Carro("Chevrolet", "Opala", 1978);
-
-            pessoa1.AdquirirCarro(carro1);
-            pessoa1.AdquirirCarro(carro2);
-            pessoa2.AdquirirCarro(carro3);
-
-            pessoa1.ListarCarros();
-            pessoa2.ListarCarros();
-
-            Console.ReadLine();
+            Console.WriteLine($"Retângulo com largura de {retangulo.Largura} e altura de {retangulo.Altura}");
+            Console.WriteLine($"Área: {retangulo.CalcularArea():F2}");
+            Console.WriteLine($"Perímetro: {retangulo.CalcularPerimetro():F2}");
         }
     }
 }
--- readme.md
+++ readme.md
@@ -1,6 +1,3 @@
-# Desafio 1
+# Desafio 2
 
-Implemente uma relação entre pessoas e carros:
-1. Crie uma classe `Pessoa` com os atributos `nome`, `idade` e `profissão`. 
-2. Crie uma classe `Carro` com os atributos `marca`, `modelo` e `ano`.
-3. Adicione elementos nessa estrutura que façam com que pessoas se tornem donas de carros.
+Crie uma classe `Retangulo` com os atributos `largura` e `altura`. Implemente um método que calcule a área do retângulo (largura x altura) e outro método que calcule o perímetro (2 x largura + 2 x altura).
